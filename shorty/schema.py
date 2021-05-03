f699490c9e5b5259489827c622704a60c7581925
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2021-05-03 19:20:52 +0530

    feat: adds graphql schema to create url objects and hashes

--- shorty/schema.py
+++ shorty/schema.py
@@ -7,4 +7,8 @@ class Query(shortener.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(shortener.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
